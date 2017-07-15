387842303faad06ff0826e9e3ff7822aadcd5f8b
Juan Naputi <jnaputi253@gmail.com>
2017-07-15 10:45:14 -0700

    Added stubbed action cases

--- JBucket/src/main/java/executors/DoubleCommandExecutor.java
+++ JBucket/src/main/java/executors/DoubleCommandExecutor.java
@@ -1,11 +1,24 @@
 package executors;
 
 import commands.Command;
+import errors.ErrorHandler;
 
 public class DoubleCommandExecutor extends BaseMultiCommandExecutor {
 	@Override
 	public void execute(Command commandToExecute) {
+		String action = commandToExecute.getAction();
 		
+		switch(action) {
+		case "create":
+			System.out.println("Bucket creation request initiated");
+			break;
+		case "delete":
+			System.out.println("Bucket cdeletion request initiated");
+			break;
+		default:
+			ErrorHandler.raiseError("invalid-command", action);
+			break;
+		}
 	}
 
 }
