137d11125497a1340ab2fef45572a2009b732411
Juan Naputi <jnaputi253@gmail.com>
2017-07-15 10:42:24 -0700

    Added the DoubleCommandExecutor class

---
+++ JBucket/src/main/java/executors/DoubleCommandExecutor.java
@@ -0,0 +1,11 @@
+package executors;
+
+import commands.Command;
+
+public class DoubleCommandExecutor extends BaseMultiCommandExecutor {
+	@Override
+	public void execute(Command commandToExecute) {
+		
+	}
+
+}
